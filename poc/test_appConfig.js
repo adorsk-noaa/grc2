29e2a203208fff49fea6165924214d71eb92dffe
adorsk <adorsk>
2013-02-03 22:19:52 -0500

    beefing up app config

--- poc/test_appConfig.js
+++ poc/test_appConfig.js
@@ -81,213 +81,111 @@ GeoRefine.initialize = function($, Backbone, _, _s){
       ]
     }
   });
-  /**************/
 
-
-  /**************/
-  var generateResultsConfigs = function(){
-    var resultFields = [
-      {
-      id: 'a',
-      label: 'A label',
-      scale_type: 'sequential',
-      colormapId: 'ColorBrewer:Rd',
-    },
-    {
-      id: 'y',
-      label: 'Y label',
-      scale_type: 'sequential',
-      colormapId: 'ColorBrewer:Pi',
-    },
-    {
-      id: 'x',
-      label: 'X label',
-      scale_type: 'sequential',
-      colormapId: 'ColorBrewer:Br',
-    },
-    {
-      id: 'z',
-      label: 'Z label',
-      scale_type: 'diverging',
-      colormapId: 'ColorBrewer:PiG',
-    },
-    {
-      id: 'znet',
-      label: 'Znet label',
-      scale_type: 'diverging',
-      colormapId: 'ColorBrewer:Rd:Bu',
-    },
-    ];
-
-    var qFields = {};
-    _.each(resultFields, function(field){
-      if (! field.col){
-        field.col = field.id;
-      }
-      var modelOpts = {};
-      var entityId = field.id + '_sum';
-      $.extend(true, modelOpts, {
-        id: field.id,
-        label: field.label,
-        format: '%.1H',
-        inner_query: {
-          SELECT: [{ID: entityId, EXPRESSION: 'func.sum(__result__' + field.col + ')'}]
-        },
-        outer_query: {
-          SELECT: [{ID: entityId + '_sum', EXPRESSION: '__inner__' + entityId}]
-        },
-      }, field);
-      qFields[field.id] = new Backbone.Model(modelOpts);
-    });
-
-    var facetDefinitions = new Backbone.Collection();
-
-    // Timestep facet.
-    facetDefinitions.add(new Backbone.Model({
-      id: 'time',
-      label: 'Time',
-      noClose: true,
-      primary_filter_groups: ['scenario'],
-      type: 'timeSlider',
-      value_type: 'numeric',
-      filter_entity: {
-        EXPRESSION: '__result__t',
-        ID: 't',
-        TYPE: 'ENTITY'
+  var generateListFacetModel = function(opts){
+    var field = opts.field;
+    var modelOpts = {};
+    var idId = field.id + '_id';
+    var labelId = field.id + '_label';
+    var tableColExpression = '__' + opts.table + '__' + idId;
+    var idColExpression = '__' + field.id + '__id';
+    var labelColExpression = '__' + field.id + '__label';
+    $.extend(true, modelOpts, {
+      info: 'da info',
+      info_link: "{{PROJECT_STATIC_DIR}}/sasipedia#" + field.sasipediaId + "/index.html",
+      inner_query: {
+        SELECT: [
+          {EXPRESSION: tableColExpression, ID: idId}
+        ],
+        GROUP_BY: [
+          {ID: idId}
+        ],
+      },
+      outer_query: {
+        SELECT: [
+          {EXPRESSION: idColExpression, ID: idId},
+          {EXPRESSION: labelColExpression, ID: labelId}
+        ],
+        FROM: [
+          {
+          SOURCE: field.id,
+          JOINS: [
+            [
+              "inner",
+              [
+                {TYPE: "ENTITY", EXPRESSION: "__inner__" + idId}, "==",
+                {TYPE: "ENTITY", EXPRESSION: idColExpression}
+              ]
+          ]
+          ]
+        }
+        ],
+        GROUP_BY: [
+          {ID: labelId},
+          {ID: idId}
+        ],
       },
       KEY: {
-        KEY_ENTITY: {EXPRESSION: '__result__t', ID: 't'},
+        LABEL_ENTITY: {ID: labelId},
         QUERY: {
-          SELECT: [{EXPRESSION: '__time__id', 'ID': 't'}]
-        }
-      },
-    }));
-
-    // Generic result facets.
-    var facetFields = [
-    {
-      id: 'substrate',
-      sasipediaId: 'substrates',
-      info: 'substrates info',
-      label: "Substrates",
-    },
-      {
-      id: 'energy',
-      sasipediaId: 'energies',
-      info: 'energy info',
-      label: "Energies",
-    },
-    {
-      id: 'feature',
-      sasipediaId: 'features',
-      info: 'feature info',
-      label: "Features",
-    },
-    {
-      id: 'feature_category',
-      sasipediaId: 'feature_categories',
-      info: 'feature category info',
-      label: "Feature Categories",
-    },
-    {
-      id: 'gear',
-      sasipediaId: 'gears',
-      info: 'gear info',
-      label: "Gear",
-    },
-    ];
-
-    _.each(facetFields, function(field){
-      var modelOpts = {};
-      var idId = field.id + '_id';
-      var labelId = field.id + '_label';
-      var resultColExpression = '__result__' + idId;
-      var idColExpression = '__' + field.id + '__id';
-      var labelColExpression = '__' + field.id + '__label';
-      $.extend(true, modelOpts, {
-        info: 'da info',
-        info_link: "{{PROJECT_STATIC_DIR}}/sasipedia#" + field.sasipediaId + "/index.html",
-        inner_query: {
-          SELECT: [
-            {EXPRESSION: resultColExpression, ID: idId}
-          ],
-          GROUP_BY: [
-            {ID: idId}
-          ],
-        },
-        outer_query: {
           SELECT: [
             {EXPRESSION: idColExpression, ID: idId},
             {EXPRESSION: labelColExpression, ID: labelId}
-          ],
-          FROM: [
-            {
-            SOURCE: field.id,
-            JOINS: [
-              [
-                "inner",
-                [
-                  {TYPE: "ENTITY", EXPRESSION: "__inner__" + idId}, "==",
-                  {TYPE: "ENTITY", EXPRESSION: idColExpression}
-                ]
-            ]
-            ]
-          }
-          ],
-          GROUP_BY: [
-            {ID: labelId},
-            {ID: idId}
-          ],
+          ]
         },
-        KEY: {
-          LABEL_ENTITY: {ID: labelId},
-          QUERY: {
-            SELECT: [
-              {EXPRESSION: idColExpression, ID: idId},
-              {EXPRESSION: labelColExpression, ID: labelId}
-            ]
-          },
-          KEY_ENTITY: {EXPRESSION: resultColExpression, ID: idId}
-        },
-        label: field.label,
-        type: "list",
-        filter_entity: {TYPE: "ENTITY", EXPRESSION: resultColExpression, ID: field.id},
-        base_filter_groups: ["scenario"],
-        primary_filter_groups: ["data"]
-      }, field);
+        KEY_ENTITY: {EXPRESSION: tableColExpression, ID: idId}
+      },
+      label: field.label,
+      type: "list",
+      filter_entity: {TYPE: "ENTITY", EXPRESSION: tableColExpression, ID: field.id},
+      base_filter_groups: ["scenario"],
+      primary_filter_groups: ["data"]
+    }, field);
 
-      facetDefinitions.add(new Backbone.Model(modelOpts));
+    return new Backbone.Model(modelOpts);
+  };
 
-    });
+  var generateDataLayerModel = function(opts){
+    var field = opts.field;
+    var densityId = field.col + '_density';
+    var propertyMappings = {};
+    propertyMappings[densityId] = densityId;
 
-
-    var dataLayers = {};
-    _.each(resultFields, function(field){
-      var densityId = field.col + '_density';
-      var propertyMappings = {};
-      propertyMappings[densityId] = densityId;
-      dataLayers[field.id] = new Backbone.Model({
-        id: field.id,
-        colormapId: field.colormapId,
-        scale_type: field.scale_type,
-        dataProp: densityId,
-        layer_type: 'Vector',
-        layer_category: 'data',
-        source: 'georefine_data',
-        label: field.label + ', per unit cell area',
+    var scaleOpts;
+    if (field.scale_type == 'sequential'){
+      scaleOpts = {
         vmin: 0,
-        vmax: 1,
-        disabled: false,
-        expanded: true,
-        base_filter_groups: ['scenario'],
-        primary_filter_groups: ['data'],
-        featuresQuery: cellsFeatureQuery,
-        propertiesQuery: new Backbone.Model({
-          quantity_field: new Backbone.Model({
-            inner_query: {
+        vmaxAuto: true,
+      };
+    }
+    else if (field.scale_type == 'diverging'){
+      scaleOpts = {
+        vmid: 0,
+        vrAuto: true,
+      };
+    }
+
+    var modelOpts = {};
+    $.extend(true, modelOpts, scaleOpts, {
+      id: field.id,
+      colormapId: field.colormapId,
+      scale_type: field.scale_type,
+      dataProp: densityId,
+      layer_type: 'Vector',
+      layer_category: 'data',
+      source: 'georefine_data',
+      label: field.label + ', per unit cell area',
+      disabled: false,
+      expanded: true,
+      base_filter_groups: ['scenario'],
+      primary_filter_groups: ['data'],
+      featuresQuery: cellsFeatureQuery,
+      propertiesQuery: new Backbone.Model({
+        quantity_field: new Backbone.Model({
+          inner_query: {
             SELECT: [
-              {ID: field.col + '_sum', EXPRESSION: 'func.sum(__result__' + field.col + ')'},
-              {ID: 'cell_id', EXPRESSION: '__result__cell_id'}
+              {ID: field.col + '_sum', EXPRESSION: 'func.sum(__' + opts.table + '__' + field.col + ')'},
+              {ID: 'cell_id', EXPRESSION: '__' + opts.table + '__cell_id'}
             ],
             GROUP_BY: [
               {ID: 'cell_id'}
@@ -308,20 +206,25 @@ GeoRefine.initialize = function($, Backbone, _, _s){
             }
             ]
           },
-          }),
-          KEY: cellKeyEntity,
-          mappings: propertyMappings,
         }),
-        styleMap: new Backbone.Collection(
-          [
-            new Backbone.Model({
-          id: 'default', 
-          strokeWidth: 0, 
-        })
-        ])
-      });
+        KEY: cellKeyEntity,
+        mappings: propertyMappings,
+      }),
+      styleMap: new Backbone.Collection(
+        [
+          new Backbone.Model({
+        id: 'default', 
+        strokeWidth: 0, 
+      })
+      ])
     });
 
+    $.extend(true, modelOpts, field);
+
+    return new Backbone.Model(modelOpts);
+  };
+
+  var generateInitialActions = function(){
     var initialTimestepFacetActionQueue = {
       "async": false,
       "type": "actionQueue",
@@ -446,8 +349,74 @@ GeoRefine.initialize = function($, Backbone, _, _s){
       ]
     };
 
+    return initialActions;
+  };
+
+  var generateDataViewConfigs = function(opts){
+    var qFields = {};
+    _.each(opts.fields, function(field){
+      if (! field.col){
+        field.col = field.id;
+      }
+      var modelOpts = {};
+      var entityId = field.id + '_sum';
+      $.extend(true, modelOpts, {
+        id: field.id,
+        label: field.label,
+        format: '%.1H',
+        inner_query: {
+          SELECT: [{ID: entityId, EXPRESSION: 'func.sum(__' + opts.table + '__' + field.col + ')'}]
+        },
+        outer_query: {
+          SELECT: [{ID: entityId + '_sum', EXPRESSION: '__inner__' + entityId}]
+        },
+      }, field);
+      qFields[field.id] = new Backbone.Model(modelOpts);
+    });
+
+    var facetDefinitions = new Backbone.Collection();
+
+    // Timestep facet.
+    var timeFacetOpts = {
+      id: 'time',
+      label: 'Time',
+      noClose: true,
+      primary_filter_groups: ['scenario'],
+      type: 'timeSlider',
+      value_type: 'numeric',
+      filter_entity: {
+        EXPRESSION: '__' + opts.table + '__t',
+        ID: 't',
+        TYPE: 'ENTITY'
+      },
+      KEY: {
+        KEY_ENTITY: {EXPRESSION: '__' + opts.table + '__t', ID: 't'},
+        QUERY: {
+          SELECT: [{EXPRESSION: '__time__id', 'ID': 't'}]
+        }
+      },
+    };
+    facetDefinitions.add(new Backbone.Model(timeFacetOpts));
+
+    _.each(opts.facetFields, function(field){
+      facetDefinitions.add(generateListFacetModel({
+        field: field,
+        table: opts.table,
+      }));
+    });
+
+    var dataLayers = {};
+    _.each(opts.fields, function(field){
+      dataLayers[field.id] = generateDataLayerModel({
+        field: field,
+        table: opts.table,
+      });
+    });
+
+    var initialActions = generateInitialActions();
+
     var dvConfigs = [];
-    _.each(resultFields, function(field){
+    _.each(opts.fields, function(field){
       dvConfigs.push(new Backbone.Model({
         label: field.label,
         qField: qFields[field.id],
@@ -480,11 +449,136 @@ GeoRefine.initialize = function($, Backbone, _, _s){
 
   /**************/
 
+
+  /**************/
+  var generateResultsConfigs = function(){
+    var dvConfigs = generateDataViewConfigs({
+      table: 'result',
+      fields: [
+        {
+        id: 'a',
+        label: 'A label',
+        scale_type: 'sequential',
+        colormapId: 'ColorBrewer:Rd',
+      },
+      {
+        id: 'y',
+        label: 'Y label',
+        scale_type: 'sequential',
+        colormapId: 'ColorBrewer:Pi',
+      },
+      {
+        id: 'x',
+        label: 'X label',
+        scale_type: 'sequential',
+        colormapId: 'ColorBrewer:Br',
+      },
+      {
+        id: 'z',
+        label: 'Z label',
+        scale_type: 'diverging',
+        colormapId: 'ColorBrewer:PiG',
+      },
+      {
+        id: 'znet',
+        label: 'Znet label',
+        scale_type: 'diverging',
+        colormapId: 'ColorBrewer:Rd:Bu',
+      },
+      ],
+      facetFields: [
+        {
+        id: 'substrate',
+        sasipediaId: 'substrates',
+        info: 'substrates info',
+        label: "Substrates",
+      },
+      {
+        id: 'energy',
+        sasipediaId: 'energies',
+        info: 'energy info',
+        label: "Energies",
+      },
+      {
+        id: 'feature',
+        sasipediaId: 'features',
+        info: 'feature info',
+        label: "Features",
+      },
+      {
+        id: 'feature_category',
+        sasipediaId: 'feature_categories',
+        info: 'feature category info',
+        label: "Feature Categories",
+      },
+      {
+        id: 'gear',
+        sasipediaId: 'gears',
+        info: 'gear info',
+        label: "Gear",
+      },
+      ]
+    });
+
+    return dvConfigs;
+  };
+
+
+  var generateFishingEffortsConfigs = function(){
+    var dvConfigs = generateDataViewConfigs({
+      table: 'econ',
+      fields: [
+        {
+        id: 'a',
+        label: 'Raw Swept Area',
+        scale_type: 'sequential',
+        colormapId: 'ColorBrewer:Rd',
+      },
+      {
+        id: 'value',
+        label: 'Value',
+        scale_type: 'sequential',
+        colormapId: 'ColorBrewer:Pi',
+      },
+      {
+        id: 'value_net',
+        label: 'Cumulative Value',
+        scale_type: 'sequential',
+        colormapId: 'ColorBrewer:Pi',
+      },
+      {
+        id: 'hours_fished_net',
+        label: 'Cumulative Hours Fished',
+        scale_type: 'sequential',
+        colormapId: 'ColorBrewer:Br',
+      },
+      ],
+      // @TODO: Add specific gear facet too?
+      facetFields: [
+        {
+        id: 'gear',
+        sasipediaId: 'gears',
+        info: 'gear info',
+        label: "Gear",
+      },
+      ],
+    });
+
+    return dvConfigs;
+  };
+
+
+  /**************/
+
   GeoRefine.config.dataViewGroups = [
     {
     label: 'SASI Results',
     models: generateResultsConfigs(),
-  }
+  },
+    {
+    label: 'Fishing Efforts',
+    models: generateFishingEffortsConfigs(),
+  },
   ];
 
   GeoRefine.config.floatingDataViews = {};
